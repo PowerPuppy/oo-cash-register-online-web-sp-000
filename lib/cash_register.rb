6b23401fdfe5b1e1dbadf476f9b7db404b1d7fa0
Chris Cheong <chrisxcheong@gmail.com>
2019-07-29 07:04:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -24,8 +24,7 @@ end
 
 def apply_discount
   if discount == 0
-    "There is no discount to apply."
-    @total
+    return "There is no discount to apply."
   else
     @total = self.total*(1-(@discount.to_f/100))
     "After the discount, the total comes to $#{@total}."
