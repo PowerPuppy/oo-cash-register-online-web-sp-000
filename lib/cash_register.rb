eab5cdb6bc4c0a14dc27124caf726155b5880c79
Chris Cheong <chrisxcheong@gmail.com>
2019-07-29 06:08:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+class CashRegister
+attr_accessor :total :discount
+def initialize(discount=0)
+  @total = 0  
+end
 
+def total
+  @total
+end
+
+def add_item(title, price,quantity=1)
+  self.total += (price*quantity)
+end
+
+
+end
