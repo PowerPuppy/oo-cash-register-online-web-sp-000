04490c2f8806cb10a2e032e5d5de16e5953a4968
Chris Cheong <chrisxcheong@gmail.com>
2019-07-29 06:11:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,10 @@ require 'pry'
 
 class CashRegister
 attr_accessor :total, :discount
-def initialize(discount=0)
+
+employee_discount = 20%
+
+def initialize(discount=employee_discount)
   @total = 0
 end
 
