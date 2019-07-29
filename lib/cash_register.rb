8d6f74ad95b4c5d68da7e0045d8d0a8dfa375627
Chris Cheong <chrisxcheong@gmail.com>
2019-07-29 07:10:55 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -27,7 +27,7 @@ def apply_discount
     "There is no discount to apply."
   else
     @total = self.total*(1-(@discount./100))
-    "After the discount, the total comes to $#{@total.}."
+    "After the discount, the total comes to $#{@total}."
   end
 end
 
